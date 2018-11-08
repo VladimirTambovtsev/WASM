40e0a32cc8637878499cff03017b2e65129fe131
Vladimir Tambovtsev <tambovcev99@mail.ru>
2018-11-08 15:37:58 +0300

    import custom memory for strings

--- public/code.c
+++ public/code.c
@@ -1,18 +1,20 @@
 #include <string.h>
 
 void numLog(int n);
-void strLog(char * msg, length);
+void strLog(char *msg, length);
 
-int main() {
-  return 42;
+int main()
+{
+    return 42;
 }
 
-
-void getDoubleNumber(int x) {
-  numLog(x * 2);
+void getDoubleNumber(int x)
+{
+    numLog(x * 2);
 }
 
-void getMessage() {
-  char * msg = "Hello from C";
-  strLog(msg, strlen(msg));
+void greet()
+{
+    char *msg = "Hello from C";
+    strLog(msg, strlen(msg));
 }
